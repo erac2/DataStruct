e2a0464d8107f7804125969323ea26ba179c47ba
erac2 <71734077+erac2@users.noreply.github.com>
2022-04-05 18:58:23 -0400

    Add files via upload

---
+++ StackConstants.java
@@ -0,0 +1,7 @@
+
+public class StackConstants {
+
+	static final String LINK = "///zorak2//users$//s1239605//Downloads/StackPalindrome.properties";
+	static final String SEPERATE = ",";
+	static final int CANT = -1;
+}
